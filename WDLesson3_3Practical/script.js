9c5c070e4bad73151cec9fde416e96fa308dba38
Jasmine <Jasminez30@nycstudents.net>
2026-01-07 19:10:20 +0000

    ekjsdbfiuwadhsdowauih 32847yqrwuew974guxz

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,20 +1,20 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
 function lebron(){
-    document.getElementById("highlight").src="https://i.pinimg.com/236x/84/69/7b/84697bc7c8ee22c32337eb3e1cbd889a.jpg";
+    document.getElementById("highlight").innerHTML ="lebron james";
 }
 
 function kirk(){
-    document.getElementById("highlight").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRirmS4PL5GtNuYs3gb0JLDFpYcvb8MWfj0Ig&s";
+    document.getElementById("highlight").innerHTML = "charlie kirk";
 }
 
 function speed(){
-    document.getElementById("highlight").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2sNdixQ3USkXf8OSSNfnhfX-H8AqHVZS_Xw&s";
+    document.getElementById("highlight").innerHTML = "speed holding laugh";
 }
 
 function pug(){
-    document.getElementById("highlight").src= "https://dogtime.com/wp-content/uploads/sites/12/2011/01/GettyImages-sb10066858aa-001-e1693353192358.jpg";
+    document.getElementById("highlight").innerHTML = "pug tongue";
 }
 
 function hit(){
-    document.getElementById("highlight").src= "https://thumbs.dreamstime.com/b/naughty-boy-reward-illustration-32250473.jpg";
+    document.getElementById("highlight").innerHTML= "hitting";
 }
