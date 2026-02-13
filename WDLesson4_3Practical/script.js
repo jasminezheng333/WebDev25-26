fa1adee19302c43f2b462751c8ad8c636156aba7
Jasmine <Jasminez30@nycstudents.net>
2026-02-13 18:59:45 +0000

    hgfjkhgfflhgfrtftg

--- WDLesson4_3Practical/script.js
+++ WDLesson4_3Practical/script.js
@@ -14,3 +14,25 @@ Guideline:
 Use past examples, classwork and practicals to assist you in completing this practical.
 */
 
+function BMI(){
+  h = parseFloat(document.getElementById('height').value);
+  w = parseFloat(document.getElementById('weight').value);
+  let op= document.getElementById("output");
+  let bmi = w/(h **2) * 703;
+  let message = "";
+  let img="";
+  if (bmi < 18.5) {
+      img = "underweight.png";
+      message ="Underweight";
+    } else if (bmi >=18.5 && bmi <25) {
+      img = "healthyweight.png";
+      message ="Healthyweight";
+    } else if (bmi >=25 && bmi<30) {
+      img = "overweight.png";
+      message ="Overweight";
+    } else if(bmi>=30) {
+      img = "obeseweight png"; 
+      message ="Obese";
+    }
+  op.innerHTML= `<img src ="${img}"><br> You are ${message}`;
+}
